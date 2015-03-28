521193de040d8e38b95490822298046cb30607c4
Johnson Lau <gatement@gmail.com>
2015-03-28 11:14:37 +0800

    stylus

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'compressor',
+
     'polls',
 )
 
@@ -88,4 +91,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = "c:/django-static"
+STATIC_ROOT = "/tmp/django-static-test"
+
+# compressor
+COMPRESS_PRECOMPILERS = (
+    ('text/stylus', 'stylus -p -c -I "%s"' % STATIC_ROOT),
+    #('text/stylus', 'stylus -p -c'),
+)
+COMPRESS_ENABLED = True
